c65dfa057224c10127b9c9b484707c67ecb917b8
sojung <sojung528@gmail.com>
2023-02-03 11:58:46 +0900

    VIEW 연결 완료

---
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -0,0 +1,29 @@
+package shop.mtcoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class BoardController {
+
+    @GetMapping({ "/", "/main" })
+    public String board() {
+        return "board/main";
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable int id) {
+        return "board/detail";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
+    }
+
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm(@PathVariable int id) {
+        return "board/updateForm";
+    }
+}
